f1e26268d3f19e857fefef2044b57069486975f0
Tien Do Thanh <dothanhtien95@gmail.com>
2026-09-02 01:37:13 +0700

    feat(api): add Scalar UI for OpenAPI documentation

--- src/BoardingHouse.Api/Program.cs
+++ src/BoardingHouse.Api/Program.cs
@@ -1,10 +1,20 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, _, _) =>
+    {
+        document.Info.Title = "Boarding House API";
+        document.Info.Version = "v1";
+        return Task.CompletedTask;
+    });
+});
 
 var app = builder.Build();
 
@@ -12,6 +22,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
